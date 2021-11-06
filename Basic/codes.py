1d412ffe283e4b201874d0f65f9d4dbe65f234fe
Jayshil <jayshilpatel147@gmail.com>
2021-11-06 19:42:11 +0530

    Final commit

---
+++ Basic/codes.py
@@ -0,0 +1,6 @@
+# This is where all the codes will live -- insteal of putting them in flk.py
+import numpy as np
+
+def sum(a,b):
+    c = np.square(a) + np.square(b)
+    return c
--- Basic/flk.py
+++ Basic/flk.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from codes import sum
+
 app = Flask(__name__, template_folder='/home/jayshil/Documents/Codes/Python/flsk/Basic')
+app.config["DEBUG"] = True
 
-
-@app.route("/")
+# Route tells the def home() will be activated when one goes to said path on web ("/" in the case below)
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('home.html')
+    errors = ""
+    if request.method == "POST":
+        number1 = None
+        number2 = None
+        try:
+            number1 = float(request.form["number1"])
+        except:
+            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number1"])
+        try:
+            number2 = float(request.form["number2"])
+        except:
+            errors += "<p>{!r} is not a number.</p>\n".format(request.form["number2"])
+        if number1 is not None and number2 is not None:
+            result = sum(number1, number2)
+            return render_template('home1.html').format(result=result)
+
+    return render_template('home.html').format(errors=errors)
 
 
 if __name__ == '__main__':
